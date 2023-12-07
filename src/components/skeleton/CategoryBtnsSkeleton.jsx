f947043d6c2a2a2e0370ce8ffd90a18757d402ce
Elcan Ali <elcan@div.edu.az>
2023-12-07 12:23:38 +0400

    first commit

--- src/components/skeleton/CategoryBtnsSkeleton.jsx
+++ src/components/skeleton/CategoryBtnsSkeleton.jsx
@@ -4,9 +4,7 @@ import { nanoid } from '@reduxjs/toolkit'
 function CategoryBtnsSkeleton(){
     return (
         <div className='gap-[15px] pt-[20px] flex flex-wrap'>
-            {Array.from({ length: 4 }).map(_ =>
-                <Skeleton key={nanoid()} variant='rounded' height={"40px"} width={"100px"} />
-            )}
+            {Array.from({ length: 4 }).map(_ =><Skeleton key={nanoid()} variant='rounded' height={"40px"} width={"100px"} />)}
         </div>
     )
 }
